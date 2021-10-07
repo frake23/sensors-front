342c342ade9e37859abedaa40f8a291462de958d
ekinohito <zhenyagur9@gmail.com>
2021-10-07 15:54:00 +0300

    Chart preparations

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import {useState} from 'react'
 import './App.css'
+import {AppBar, Box, Container, Toolbar, Typography} from "@mui/material";
+import {LineChart, CartesianGrid, Line, XAxis, YAxis, ResponsiveContainer} from "recharts";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <>
+            <AppBar position="sticky">
+                <Container>
+                    <Toolbar>
+                        <Typography variant="h4">Графики</Typography>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Container>
+                <Box display="flex" flexDirection="column" alignItems="center" marginTop="30px">
+                    <Typography variant="h3" textAlign="center">Поле для вывода графиков</Typography>
+                    <ResponsiveContainer width="100%" height={400} minWidth={200}>
+                        <LineChart data={[{x: 1, y:2}, {x: 2, y: 4}, {x: 3, y: 3}]} margin={{ top: 40, right: 30, left: 0, bottom: 0 }}>
+                            <Line type="monotone" dataKey="y" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" />
+                            <XAxis dataKey="x" />
+                            <YAxis />
+                        </LineChart>
+                    </ResponsiveContainer>
+
+                </Box>
+            </Container>
+        </>
+    )
 }
 
 export default App
