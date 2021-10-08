25382fbbe7e5568e90dd8b8d6fbd073ba10c2cce
ekinohito <zhenyagur9@gmail.com>
2021-10-08 19:42:19 +0300

    Plot changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import {useState} from 'react'
 import './App.css'
 import {
     AppBar,
-    Box, Button,
-    Container, FormControl,
-    Grid,
-    Input, InputLabel,
+    Box,
+    Button,
+    Container,
+    FormControl,
+    InputLabel,
     MenuItem,
-    Select,
     Stack,
-    TextField,
     Toolbar,
     Typography
 } from "@mui/material";
-import {LineChart, CartesianGrid, Line, XAxis, YAxis, ResponsiveContainer} from "recharts";
-import { toast, ToastContainer } from 'react-toastify';
-import {Controller, useForm} from 'react-hook-form';
+import {CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {ToastContainer} from 'react-toastify';
+import {useForm} from 'react-hook-form';
 import usePoints from './usePoints';
-import { TextInput, SelectInput } from './Input';
+import {SelectInput, TextInput} from './Input';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     const {control, handleSubmit} = useForm();
     const {points, generatePoints} = usePoints();
+    const [animationDuration, setAnimationDuration] = useState(1000)
     return (
         <>
             <AppBar position="sticky">
@@ -36,36 +36,37 @@ function App() {
                 <Box display="flex" flexDirection="column" alignItems="center" marginTop="30px">
                     <Typography variant="h3" textAlign="center">Поле для вывода графиков</Typography>
                     <ResponsiveContainer width="100%" height={400} minWidth={200}>
-                        <LineChart data={points} margin={{ top: 40, right: 30, left: 0, bottom: 0 }}>
-                            <Line type="monotone" dataKey="y" stroke="#8884d8" />
-                            <CartesianGrid stroke="#ccc" />
-                            <XAxis dataKey="x" />
-                            <YAxis />
+                        <LineChart data={points} margin={{top: 40, right: 30, left: 0, bottom: 0}}>
+                            <Line type="monotone" dataKey="y" stroke="#8884d8" animationDuration={animationDuration} isAnimationActive={false}/>
+                            <CartesianGrid stroke="#ccc"/>
+                            <XAxis dataKey="x" type="number" domain={[0, 100]}/>
+                            <YAxis/>
                         </LineChart>
                     </ResponsiveContainer>
-                    <form onSubmit={handleSubmit((values: any) => generatePoints(values))}>
+                    <form onSubmit={handleSubmit((values: any) => { setAnimationDuration(values.sleep * 1000 || 1000); generatePoints(values) })}>
                         <Stack spacing={3}>
-                        <Stack direction="row" spacing={3}>
-                            <TextInput label="Минимум х" name="minX" control={control}/>
-                            <TextInput label="Максимум х" name="maxX" control={control}/>
-                            <TextInput label="Количество точек" name="quantity" control={control}/>
-                            <TextInput label="Интервал передачи" name="sleep" control={control}/>
-                        </Stack>
-                        <Stack direction="row" spacing={3}>
-                            <TextInput label="Минимум y" name="minY" control={control}/>
-                            <TextInput label="Максимум х" name="maxY" control={control}/>
-                            <FormControl sx={{flexGrow: 1}}>
-                                <InputLabel id="select-random">Характер случайности</InputLabel>
-                                <SelectInput label="Характер случайности" name="randomType" control={control}>
-                                    <MenuItem value="uniform">Uniform</MenuItem>
-                                    <MenuItem value="smart">Smart</MenuItem>
-                                </SelectInput>
-                            </FormControl>
-                            <Button type="submit" variant="contained" color="primary" sx={{paddingX: "60px"}}>Начать!</Button>
-                        </Stack>
+                            <Stack direction="row" spacing={3}>
+                                <TextInput label="Минимум х" name="min_x" control={control}/>
+                                <TextInput label="Максимум х" name="max_x" control={control}/>
+                                <TextInput label="Количество точек" name="quantity" control={control}/>
+                                <TextInput label="Интервал передачи" name="sleep" control={control}/>
+                            </Stack>
+                            <Stack direction="row" spacing={3}>
+                                <TextInput label="Минимум y" name="min_y" control={control}/>
+                                <TextInput label="Максимум y" name="max_y" control={control}/>
+                                <FormControl sx={{flexGrow: 1}}>
+                                    <InputLabel id="select-random">Характер случайности</InputLabel>
+                                    <SelectInput label="Характер случайности" name="random" control={control}>
+                                        <MenuItem value="uniform">Uniform</MenuItem>
+                                        <MenuItem value="smart">Smart</MenuItem>
+                                    </SelectInput>
+                                </FormControl>
+                                <Button type="submit" variant="contained" color="primary"
+                                        sx={{paddingX: "60px"}}>Начать!</Button>
+                            </Stack>
                         </Stack>
                     </form>
-                    
+
 
                 </Box>
             </Container>
